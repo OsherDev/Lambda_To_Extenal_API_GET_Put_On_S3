4097d087af83ba9821332c90312002232cdae242
Osher Cohen <88276964+OsherDev@users.noreply.github.com>
2022-11-14 16:09:04 +0200

    Create Lambda_Api_Retrieve.py

---
+++ Lambda_Api_Retrieve.py
@@ -0,0 +1,23 @@
+#First of all need to add a layer with reequest package
+#mkdir lambda_layers
+#cd lambda_layers
+#mkdir python
+#cd python
+#pip install requests -t ./
+#cd ..
+#zip -r python_modules.zip .
+
+import json
+import boto3
+import requests
+import os
+
+access_key = os.environ["Access_Key"]
+
+def lambda_handler(event, context):
+    #Var
+    access_key = os.environ["Access_Key"]
+    # URL
+    url = f'SOME_URLaccess_key={access_key}'
+    response = requests.get(url)
+    json = response.json()
