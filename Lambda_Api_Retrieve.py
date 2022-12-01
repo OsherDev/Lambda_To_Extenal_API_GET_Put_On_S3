5b805c04b6a46ec47997bdb284a790aa324ab213
Osher Cohen <88276964+OsherDev@users.noreply.github.com>
2022-12-01 11:18:27 +0200

    Update Lambda_Api_Retrieve.py

--- Lambda_Api_Retrieve.py
+++ Lambda_Api_Retrieve.py
@@ -21,3 +21,9 @@ def lambda_handler(event, context):
     url = f'SOME_URLaccess_key={access_key}'
     response = requests.get(url)
     json = response.json()
+    
+    #if response successful then continue
+        if response.status_code == 200 :
+            parse_json_data = json.loads(response.text)
+            s3_file = os.environ["File_Name"] + '_' + base_currency
+            s3.Bucket(bucket_name).put_object(Key=s3_file, Body=json.dumps(parse_json_data))
